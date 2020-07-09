eb0b6e899204b319a9fa6b97be452a07503cdfbc
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-09 13:25:41 +0100

    Added TflClient class

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using RestSharp;
 using RestSharp.Authenticators;
 
 namespace BusBoard.ConsoleApp
@@ -14,24 +12,13 @@ namespace BusBoard.ConsoleApp
     const int numberBusesToDisplay = 5;
     static void Main(string[] args)
     {
-      ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-      //490008660N
       BusBoardInput inputSystem = new ConsoleBusBoardInput();
       string userInput = inputSystem.GetStringInput();
-      
-      var client = new RestClient("https://api.tfl.gov.uk/");
-      client.Authenticator = new HttpBasicAuthenticator("username", "password");
-      
-      var request = new RestRequest($"StopPoint/{userInput}/Arrivals", Method.GET);
-      request.AddUrlSegment("app_id", "1ca74190");
-      request.AddUrlSegment("app_key", "ad2ece2581024fa28abe323c0d4109c7");
-      
-      var response = client.Execute(request);
+      //todo: validate the stop code
+      //Example stop code: 490008660N
 
-      var busList = client.Get<List<BusInfo>>(request).Data;
-
+      List<BusInfo> busList = TfLClient.GetBusesAtStopCode(userInput);
       busList.Sort((x, y) => x.TimeToStation.CompareTo(y.TimeToStation));
-      
 
       foreach (BusInfo busInfo in busList.Take(numberBusesToDisplay))
       {
---
+++ BusBoard.ConsoleApp/TfLClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using RestSharp;
+
+namespace BusBoard.ConsoleApp
+{
+    public static class TfLClient
+    {
+        static RestClient client = new RestClient("https://api.tfl.gov.uk/"); 
+        //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+        public static List<BusInfo> GetBusesAtStopCode(string stopCode)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+      
+            var request = new RestRequest($"StopPoint/{stopCode}/Arrivals", Method.GET);
+            request.AddUrlSegment("app_id", "1ca74190");
+            request.AddUrlSegment("app_key", "ad2ece2581024fa28abe323c0d4109c7");
+      
+            var response = client.Execute(request);
+
+            return client.Get<List<BusInfo>>(request).Data;
+        }
+    }
+}
